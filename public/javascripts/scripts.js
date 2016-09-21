47849d21a5f3e073e9baf4c9c78e8c1396d4a6ec
Tim Pender <mrtimpender@gmail.com>
2016-09-21 16:08:09 -0600

    typist working

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,52 @@
-// alert('hello');
-if (typeof WOW == "function")
-    new WOW().init();
+;(function () {
+    "use strict";
+
+    var $window, $document, $body;
+
+    $window = $(window);
+    $document = $(document);
+    $body = $("body");
+
+
+    /*==============================================
+     Pre loader init
+     ===============================================*/
+    // $window.on("load", function () {
+    //     $body.imagesLoaded(function () {
+    //         $(".tb-preloader-wave").fadeOut();
+    //         $("#tb-preloader").delay(200).fadeOut("slow").remove();
+    //     });
+    // });
+    $document.ready(function () {
+
+    /* WOW Ini */
+    if (typeof WOW == "function")
+        new WOW().init();
+
+    /* Typist Init */
+    // if (typeof Typist == "function") {
+    //     new Typist(document.querySelector(".typist-element"), {
+    //         letterInterval: 60,
+    //         textInterval: 3000
+    //     });
+    // }
+    $('.typist')
+      .typist({
+          letterInterval: 80,
+          text: ''
+      })
+      .typistAdd('Develops ')
+      .typistPause(1500)
+      .typistRemove(('develops ').length)
+      .typistAdd('Designs ')
+      .typistPause(1500)
+      .typistRemove(('designs ').length)
+      .typistAdd('Troubleshoots ')
+      .typistPause(1500)
+      .typistRemove(('Troubleshoots ').length)
+      .typistAdd('Enjoys every minute of it all.')
+
+
+
+  });
+})(jQuery);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'timpending' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'timpending' });
+// });
+
+router.get('/test', function(req, res, next) {
+  res.render('index')
 });
 
 module.exports = router;
